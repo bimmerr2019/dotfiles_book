e273d9c6546cea28654a3e43090c40c087823d18
bimmerr2019 <bimmer@Interamnia>
2022-10-30 16:09:22 -0700

    test

--- install/install-zsh.sh
+++ install/install-zsh.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 mkdir -p "$ZDOTDIR"
+mkdir -p "$ZDOTDIR/prompt"
 
 ln -sf "$DOTFILES/zsh/zshenv" "$HOME/.zshenv"
 ln -sf "$DOTFILES/zsh/zshrc" "$ZDOTDIR/.zshrc"
 
 ln -sf "$DOTFILES/zsh/dircolors" "$ZDOTDIR/dircolors"
 
+git clone https://github.com/phantas0s/purification "$ZDOTDIR/prompt"
